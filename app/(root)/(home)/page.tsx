3aaaa131ac89b450638ab9347003feadee606d7b
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-11-23 00:00:03 +0600

    fix deployment error

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,7 +4,7 @@ import { getDuas } from '@/lib/actions/dua.action';
 import { SearchParamsProps } from '@/types';
 
 export default async function Home({ searchParams }: SearchParamsProps) {
-  const { cat, subcat, dua } = await searchParams;
+  const { cat, subcat, dua } = searchParams;
   const duas = await getDuas({
     cat: cat || '',
     subcat: subcat || '',
